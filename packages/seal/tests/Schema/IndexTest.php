62b37d85d2d1d2e7455164a8ad422171dfd02f23
Alexander Schranz <alexander@sulu.io>
2024-11-03 22:14:30 +0100

    Add tests for DSN parsing in AdapterFactory (#454)
    To make https://github.com/schranz-search/schranz-search/pull/452 better
    testable I added a few tests to for the AdapterFactory parseDsn part.

--- packages/seal/tests/Schema/IndexTest.php
+++ packages/seal/tests/Schema/IndexTest.php
@@ -13,11 +13,13 @@ declare(strict_types=1);
 
 namespace Schranz\Search\SEAL\Tests\Schema;
 
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use Schranz\Search\SEAL\Schema\Field;
 use Schranz\Search\SEAL\Schema\Index;
 
+#[CoversClass(Index::class)]
 class IndexTest extends TestCase
 {
     public function testIndex(): void
