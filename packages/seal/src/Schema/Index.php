47beb513f8745a57a4ecc5c7f18c9676ccf35c84
Alexander Schranz <alexander@sulu.io>
2025-04-01 13:07:17 +0200

    Fix handling of typed fields for getFieldByPath

--- packages/seal/src/Schema/Index.php
+++ packages/seal/src/Schema/Index.php
@@ -85,12 +85,13 @@ final class Index
 
         while (true) {
             $field = $fields[\current($pathParts)] ?? null;
+            \next($pathParts);
 
             if ($field instanceof TypedField) {
                 $fields = $field->types[\current($pathParts)];
+                \next($pathParts);
             } elseif ($field instanceof ObjectField) {
                 $fields = $field->fields;
-                \next($pathParts);
             } elseif ($field instanceof AbstractField) {
                 return $field;
             } else {
--- packages/seal/tests/Schema/IndexTest.php
+++ packages/seal/tests/Schema/IndexTest.php
@@ -103,13 +103,10 @@ class IndexTest extends TestCase
         ]);
     }
 
-    public function testGetObjectFieldByPath(): void
+    public function testGetFieldByPathObject(): void
     {
         $index = new Index('test', [
             'uuid' => new Field\IdentifierField('uuid'),
-            'title_underline' => new Field\TextField('title_underline'),
-            'descriptionCamelCase' => new Field\TextField('descriptionCamelCase'),
-            'number01' => new Field\TextField('number01'),
             'object' => new Field\ObjectField('object', [
                 'name' => new Field\TextField('name'),
             ]),
@@ -118,6 +115,28 @@ class IndexTest extends TestCase
         $field = $index->getFieldByPath('object.name');
         $this->assertInstanceOf(Field\TextField::class, $field);
         $this->assertSame('name', $field->name);
+    }
+
+    public function testGetFieldByPathType(): void
+    {
+        $index = new Index('test', [
+            'uuid' => new Field\IdentifierField('uuid'),
+            'blocks' => new Field\TypedField('blocks', 'type', [
+                'text' => [
+                    'title' => new Field\TextField('title'),
+                    'description' => new Field\TextField('description'),
+                    'media' => new Field\IntegerField('media', multiple: true),
+                ],
+                'embed' => [
+                    'title' => new Field\TextField('title'),
+                    'media' => new Field\TextField('media', searchable: false),
+                ],
+            ], multiple: true),
+        ]);
+
+        $field = $index->getFieldByPath('blocks.text.media');
+        $this->assertInstanceOf(Field\IntegerField::class, $field);
+        $this->assertSame('media', $field->name);
     }
 
     /**
