6ac956fe71f7b5928198d93b5e01bc65ec5179f0
Yanick Witschi <yanick.witschi@terminal42.ch>
2025-05-26 17:35:30 +0200

    Try Solr

--- packages/seal-solr-adapter/src/SolrSearcher.php
+++ packages/seal-solr-adapter/src/SolrSearcher.php
@@ -109,6 +109,11 @@ final class SolrSearcher implements SearcherInterface
             $highlighting->setSimplePostfix($search->highlightPostTag);
         }
 
+        if (null !== $search->index->getDistinctAttribute()) {
+            $grouping = $query->getGrouping();
+            $highlighting->setFields($search->index->getDistinctAttribute());
+        }
+        
         $result = $this->client->select($query);
 
         return new Result(
