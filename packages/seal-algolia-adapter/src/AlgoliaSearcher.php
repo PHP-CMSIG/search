0c4590130320a76a88dbd8f0da9cc8e0e0b6a1e9
Yanick Witschi <yanick.witschi@terminal42.ch>
2025-06-25 10:50:52 +0200

    CS

--- packages/seal-algolia-adapter/src/AlgoliaSearcher.php
+++ packages/seal-algolia-adapter/src/AlgoliaSearcher.php
@@ -41,7 +41,10 @@ final class AlgoliaSearcher implements SearcherInterface
 
     public function count(Index $index): int
     {
-        return (int) $this->client->searchSingleIndex($index->name)['nbHits'] ?? 0;
+        $data = $this->client->searchSingleIndex($index->name);
+        \assert(isset($data['nbHits']) && \is_int($data['nbHits']), 'The "nbHits" value is expected to be returned by algolia client.');
+
+        return $data['nbHits'];
     }
 
     public function search(Search $search): Result
