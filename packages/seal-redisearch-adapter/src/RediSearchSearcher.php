bb580e3a9f60bc813f8e9e66d32a87ba40d21377
Yanick Witschi <yanick.witschi@terminal42.ch>
2025-07-22 17:55:18 +0200

    Half of the redis implementation

--- packages/seal-redisearch-adapter/src/RediSearchSearcher.php
+++ packages/seal-redisearch-adapter/src/RediSearchSearcher.php
@@ -18,6 +18,8 @@ use CmsIg\Seal\Marshaller\Marshaller;
 use CmsIg\Seal\Schema\Field;
 use CmsIg\Seal\Schema\Index;
 use CmsIg\Seal\Search\Condition;
+use CmsIg\Seal\Search\Facet\CountFacet;
+use CmsIg\Seal\Search\Facet\MinMaxFacet;
 use CmsIg\Seal\Search\Result;
 use CmsIg\Seal\Search\Search;
 
@@ -199,6 +201,7 @@ final class RediSearchSearcher implements SearcherInterface
         return new Result(
             $this->hitsToDocuments($search->index, $documents),
             $total,
+            $this->addFacets($search, $query, $parameters),
         );
     }
 
@@ -308,5 +311,67 @@ final class RediSearchSearcher implements SearcherInterface
         }
 
         return \implode($conjunctive ? ' ' : ' | ', $filters);
+    }
+
+    /**
+     * @param array<string, string> $parameters
+     */
+    private function addFacets(Search $search, string $query, array $parameters): array
+    {
+        $formatted = [];
+
+        foreach ($search->facets as $facet) {
+            $arguments = [];
+
+            if ([] !== $parameters) {
+                $arguments[] = 'PARAMS';
+                $arguments[] = \count($parameters) * 2;
+                foreach ($parameters as $key => $value) {
+                    $arguments[] = $key;
+                    $arguments[] = $value;
+                }
+            }
+
+            if ($facet instanceof MinMaxFacet) {
+                $arguments = \array_merge($arguments, [
+                    'GROUPBY', '0',
+                    'REDUCE', 'MIN', '1', '@' . $facet->field, 'AS', 'min_' . $this->getFilterField($search->index, $facet->field),
+                    'REDUCE', 'MAX', '1', '@' . $facet->field, 'AS', 'max_' . $this->getFilterField($search->index, $facet->field),
+                ]);
+
+                $arguments[] = 'DIALECT';
+                $arguments[] = '3';
+
+                /** @var mixed[]|false $result */
+                $result = $this->client->rawCommand('FT.AGGREGATE', $search->index->name, $query, ...$arguments);
+
+                if (isset($result[1]) && \is_array($result[1])) {
+                    $formatted[$facet->field] = [
+                        'min' => (float) $result[1][1],
+                        'max' => (float) $result[1][3],
+                    ];
+                }
+            }
+
+            if ($facet instanceof CountFacet) {
+                $arguments = \array_merge($arguments, [
+                    'GROUPBY', '1', '@' . $this->getFilterField($search->index, $facet->field),
+                    'REDUCE', 'COUNT', '0', 'AS', 'count',
+                ]);
+
+                $arguments[] = 'DIALECT';
+                $arguments[] = '3';
+
+                /** @var mixed[]|false $result */
+                $result = $this->client->rawCommand('FT.AGGREGATE', $search->index->name, $query, ...$arguments);
+                $counts = [];
+
+                // TODO: no idea why tags does not work properly here...
+
+                $formatted[$facet->field] = $counts;
+            }
+        }
+
+        return $formatted;
     }
 }
