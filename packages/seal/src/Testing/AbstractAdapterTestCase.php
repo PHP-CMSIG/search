3708852e2ff0fe07aa7653bbac5298ee90993b75
Alexander Schranz <alexander@sulu.io>
2026-04-07 20:08:24 +0200

    Add test for DynamicReindexProviderInterface

--- packages/seal/src/Testing/AbstractAdapterTestCase.php
+++ packages/seal/src/Testing/AbstractAdapterTestCase.php
@@ -19,6 +19,7 @@ use CmsIg\Seal\EngineInterface;
 use CmsIg\Seal\Exception\DocumentNotFoundException;
 use CmsIg\Seal\Reindex\ReindexConfig;
 use CmsIg\Seal\Reindex\ReindexProviderInterface;
+use CmsIg\Seal\Reindex\DynamicReindexProviderInterface;
 use CmsIg\Seal\Schema\Schema;
 use PHPUnit\Framework\TestCase;
 
@@ -212,6 +213,65 @@ abstract class AbstractAdapterTestCase extends TestCase
         self::$taskHelper->waitForAll();
     }
 
+    public function testDynamicReindex(): void
+    {
+        $engine = self::getEngine();
+        $task = self::getEngine()->createSchema(['return_slow_promise_result' => true]);
+        $task->wait();
+
+        $removedDocument = [
+            'uuid' => '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+            'title' => 'Removed Document',
+        ];
+
+        $documents = [
+            ...TestingHelper::createComplexFixtures(),
+            $removedDocument,
+        ];
+
+        $reindexProvider = $this->createDynamicReindexProvider($documents);
+        $engine->reindex([$reindexProvider], new ReindexConfig(), null, ['return_slow_promise_result' => true])->wait(); // @phpstan-ignore-line
+
+        $expectedDocuments = [];
+        foreach ($documents as $document) {
+            $expectedDocuments[] = $engine->getDocument(TestingHelper::INDEX_COMPLEX, $document['uuid']);
+        }
+
+        unset($expectedDocuments[\count($expectedDocuments) - 1]);
+
+        $this->assertCount(
+            \count($documents) - 1,
+            $expectedDocuments,
+        );
+
+        $reindexProvider = $this->createDynamicReindexProvider($expectedDocuments);
+        $reindexConfig = (new ReindexConfig())
+            ->withIndex(TestingHelper::INDEX_COMPLEX)
+            ->withIdentifiers(
+                \array_map(
+                    static fn ($document) => $document['uuid'],
+                    $documents,
+                ),
+            );
+        $engine->reindex([$reindexProvider], $reindexConfig, null, ['return_slow_promise_result' => true])->wait(); // @phpstan-ignore-line
+
+        $exception = null;
+        try {
+            $engine->getDocument(TestingHelper::INDEX_COMPLEX, '3fa85f64-5717-4562-b3fc-2c963f66afa6');
+        } catch (\Exception $e) {
+            $exception = $e;
+        }
+
+        $this->assertInstanceOf(DocumentNotFoundException::class, $exception);
+
+        foreach ($expectedDocuments as $document) {
+            \assert(\is_string($document['uuid']), 'UUID is not a string');
+            self::$taskHelper->tasks[] = $engine->deleteDocument(TestingHelper::INDEX_COMPLEX, $document['uuid'], ['return_slow_promise_result' => true]);
+        }
+
+        self::$taskHelper->waitForAll();
+    }
+
     public function testCountDocuments(): void
     {
         $engine = self::getEngine();
@@ -267,6 +327,43 @@ abstract class AbstractAdapterTestCase extends TestCase
             public static function getIndex(): string
             {
                 return TestingHelper::INDEX_COMPLEX;
+            }
+        };
+    }
+
+    /**
+     * @param array<array<string, mixed>> $documents
+     */
+    private function createDynamicReindexProvider(array $documents): DynamicReindexProviderInterface
+    {
+        return new class($documents) implements DynamicReindexProviderInterface {
+            /**
+             * @param array<array<string, mixed>> $documents
+             */
+            public function __construct(private readonly array $documents)
+            {
+            }
+
+            public function total(string $index): int
+            {
+                if (TestingHelper::INDEX_COMPLEX !== $index) {
+                    return 0;
+                }
+
+                return 4;
+            }
+
+            public function provide(string $index, ReindexConfig $reindexConfig): \Generator
+            {
+                if (TestingHelper::INDEX_COMPLEX !== $index) {
+                    return;
+                }
+
+                $documents = $this->documents;
+
+                foreach ($documents as $document) {
+                    yield $document;
+                }
             }
         };
     }
